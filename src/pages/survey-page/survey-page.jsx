8cbb39d3cb9b555f273ac6a9a0459a6e9becddd5
PHUICMT <PHUICMT@github.com>
2022-08-31 09:59:24 +0700

    Implement number of item

--- src/pages/survey-page/survey-page.jsx
+++ src/pages/survey-page/survey-page.jsx
@@ -19,15 +19,15 @@ export function SurveyPage() {
     return (
         <Container>
             <Header />
-            <RowRadioButtonsGroup title="เบื่อ ทำอะไร ๆ ก็ไม่เพลิดเพลิน" />
-            <RowRadioButtonsGroup title="ไม่สบายใจ ซึมเศร้า หรือท้อแท้" />
-            <RowRadioButtonsGroup title="หลับยาก หรือหลับ ๆ ตื่น ๆ หรือหลับมากไป" />
-            <RowRadioButtonsGroup title="เหนื่อยง่าย หรือไม่ค่อยมีแรง" />
-            <RowRadioButtonsGroup title="เบื่ออาหาร หรือกินมากเกินไป" />
-            <RowRadioButtonsGroup title="รู้สึกไม่ดีกับตัวเอง คิดว่าตัวเองล้มเหลว หรือเป็นคนทำให้ตัวเอง หรือครอบครัวผิดหวัง" />
-            <RowRadioButtonsGroup title="สมาธิไม่ดีเวลาทำอะไร เช่น ดูโทรทัศน์ ฟังวิทยุ หรือทำงานที่ต้องใช้ความตั้งใจ" />
-            <RowRadioButtonsGroup title="พูดหรือทำอะไรช้าจนคนอื่นมองเห็น หรือกระสับกระส่ายจนท่านอยู่ไม่นิ่งเหมือนเคย" />
-            <RowRadioButtonsGroup title="คิดทำร้ายตนเอง หรือคิดว่าถ้าตาย ๆ ไปเสียคงจะดี" />
+            <RowRadioButtonsGroup title="1.เบื่อ ทำอะไร ๆ ก็ไม่เพลิดเพลิน" />
+            <RowRadioButtonsGroup title="2.ไม่สบายใจ ซึมเศร้า หรือท้อแท้" />
+            <RowRadioButtonsGroup title="3.หลับยาก หรือหลับ ๆ ตื่น ๆ หรือหลับมากไป" />
+            <RowRadioButtonsGroup title="4.เหนื่อยง่าย หรือไม่ค่อยมีแรง" />
+            <RowRadioButtonsGroup title="5.เบื่ออาหาร หรือกินมากเกินไป" />
+            <RowRadioButtonsGroup title="6.รู้สึกไม่ดีกับตัวเอง คิดว่าตัวเองล้มเหลว หรือเป็นคนทำให้ตัวเอง หรือครอบครัวผิดหวัง" />
+            <RowRadioButtonsGroup title="7.สมาธิไม่ดีเวลาทำอะไร เช่น ดูโทรทัศน์ ฟังวิทยุ หรือทำงานที่ต้องใช้ความตั้งใจ" />
+            <RowRadioButtonsGroup title="8.พูดหรือทำอะไรช้าจนคนอื่นมองเห็น หรือกระสับกระส่ายจนท่านอยู่ไม่นิ่งเหมือนเคย" />
+            <RowRadioButtonsGroup title="9.คิดทำร้ายตนเอง หรือคิดว่าถ้าตาย ๆ ไปเสียคงจะดี" />
             <Stack spacing={2} alignItems="center">
                 <InputEmailField
                     placeholder="โปรดระบุอีเมลของคุณ"
